89a5fe95e3c08c3c804f2c20a93c8a6731ba20a0
kangsulim <rkdtnfla1@naver.com>
2025-05-26 15:39:09 +0900

    3

---
+++ ch5/gitapi/src/App.tsx
@@ -0,0 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Repositories from './components/Repositories';
+import './App.css'
+
+function App() {
+
+    const queryClient = new QueryClient();
+    
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Repositories></Repositories>
+      </QueryClientProvider>
+    </>
+  )
+}
+
+export default App
