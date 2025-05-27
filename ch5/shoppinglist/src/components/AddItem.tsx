0e53ab0f9daedeb856663cb1df7530e9dfde016a
kangsulim <rkdtnfla1@naver.com>
2025-05-27 12:49:30 +0900

    shoppingList doen

--- ch5/shoppinglist/src/components/AddItem.tsx
+++ ch5/shoppinglist/src/components/AddItem.tsx
@@ -3,7 +3,11 @@ import { Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material
 import { useState } from "react";
 import { Item } from "../App";
 
-export default function AddItem() {
+type AddItemProps = {
+  addItem: (item: Item) => void;
+}
+
+export default function AddItem(props) {
   const [ open, setOpen ] = useState(false);
   const [ item, setItem ] = useState({
     product: '',
@@ -17,10 +21,17 @@ export default function AddItem() {
   const handleClose = () => {
     setOpen(false);
   }
+  
+  const addItem = () => {
+    props.addItem(item);
+
+    setItem({ product: '', amount: ''});
+    handleClose();
+  }
 
   return(
     <>
-      <Button onClick={handleOpen}>
+      <Button onClick={handleOpen} variant="text">
         Add Item / 항목 추가
       </Button>
       <Dialog open={open} onClose={handleClose}>
@@ -29,16 +40,19 @@ export default function AddItem() {
           <TextField value={item.product} margin="dense"
             onChange={ e => setItem({...item, product: e.target.value}) }
             label="Product/제품" fullWidth />
+          <TextField value={item.amount} margin="dense"
+            onChange={ e => setItem({...item, amount: e.target.value}) }
+            label="Amount/제품" fullWidth />
 
             {/* 동일한 형태로 input을 amount에 맞춰서 쓸 수 있겠네요 */}
             
         </DialogContent>
         <DialogActions >
-          <Button onClick={handleClose}>
+          <Button onClick={handleClose} variant="outlined">
             Cancel / 취소
           </Button>
 
-          <Button>
+          <Button onClick={addItem} variant="outlined">
             Add / 추가
           </Button>
         </DialogActions>
