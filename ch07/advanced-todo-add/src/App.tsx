265a45670049b4c5877c99e41c9fe5f0e47e1e20
kangsulim <rkdtnfla1@naver.com>
2025-06-04 16:18:11 +0900

    done

--- ch07/advanced-todo-add/src/App.tsx
+++ ch07/advanced-todo-add/src/App.tsx
@@ -3,6 +3,7 @@ import TodoForm from './components/TodoForm'
 import TodoList from './components/TodoList';
 import type { Todo } from './types/Todo';
 import { v4 as uuid } from 'uuid';
+import './App.css';
 
 function App() {
   const [todos,setTodos] = useState<Todo[]>(() => {
@@ -25,23 +26,25 @@ function App() {
   }
 
   const deleteTodo = (id: string) => {
-    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    const updatedTodos = todos.filter((todo) => todo.id !== id);  // 삭제할 때의 공식(정석), 삭제할 id를 제외한 나머지 id를 가지고 있는 요소를 가져옴
     setTodos(updatedTodos);
     localStorage.setItem("todos", JSON.stringify(updatedTodos));
   }
 
   const toggleComplete = (id: string) => {
     const updatedTodos = todos.map((todo) =>
-      todo.id === id ? { ...todo, completed: !todo.completed } : todo,
-    );
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo,);
+    
     console.log("업데이트 전 todos:", todos);
     console.log("업데이트 후 todos:", updatedTodos);
+
     setTodos(updatedTodos);
     localStorage.setItem("todos", JSON.stringify(updatedTodos));
-  };
+  }
 
   return (
     <>
+      <h1>Todo List</h1>
       <TodoForm onAddTodo={addTodo}></TodoForm>
       <TodoList todos={todos} onDeleteTodo={deleteTodo} onToggleComplete={toggleComplete}></TodoList>
     </>
